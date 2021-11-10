e6e019e0760ad7206f531e0c8adc3b61b05744e3
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-10 17:29:24 +0100

    work in progress

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/**
+ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+ */
+
+
+
+
+// Referenze
+
+ const countdown = document.querySelector('.countdown');
+const display = document.querySelector('.display');
+const number = document.querySelector('.number');
+
+let secondi = 5;
+
+
+
+// array di numeri casuali
+const numberArray = [];
+
+
+// generazione numeri casuali
+for(let i = 0; i < 5; i++) {
+  numberArray.push( randomNum() );
+};
+
+number.innerHTML = numberArray;
+
+
+
+const timer = setInterval(() => {
+
+    
+    // raggiunto lo zero
+    if(secondi === 0) {
+        // fermare intervallo di loop
+        clearInterval(timer)
+    
+        // aggiornare display
+        display.innerHTML = '';
+        
+    } else {
+        countdown.innerText = secondi
+        secondi--;
+    }
+    
+}, 1000);  
+
+
+
+
+/*************************************************
+                    FUNZIONI
+ *************************************************/
+
+// generzione random di 5 numeri
+
+ function randomNum(){
+    return Math.floor(Math.random() * 100) + 1;
+} 
+
+console.log(numeriRandom);
